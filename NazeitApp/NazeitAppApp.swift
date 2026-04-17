80269360b45e2e7b06139713c454a7e12039e73c
fakhrizamaris <djamarisfakhri@gmail.com>
2026-04-17 19:44:18 +0700

    Initial Commit

---
+++ NazeitApp/NazeitAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  NazeitAppApp.swift
+//  NazeitApp
+//
+//  Created by Fakhri Djamaris on 17/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NazeitAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
